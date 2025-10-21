7845b71b6454dcbb39de2a7f3c39becdb6b2327d
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2025-10-21 10:59:15 +0300

    Add HeroSection component to the Home page layout

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
+import HeroSection from "./components/landing-page/hero-section";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-[#DCFFE8]/30">
       <Navbar/>
+      <HeroSection/>
       <Footer/>
     </main>
   );
